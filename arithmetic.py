0148b7ae745ce178af6a764f09e30384b495e844
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:31:11 -0700

    added docstring, fixed divide function

--- arithmetic.py
+++ arithmetic.py
@@ -1,23 +1,31 @@
 def add(num1, num2):
-    return num1 + num2 
+	"""adds two numbers together"""
+	return num1 + num2 
 
 def subtract(num1, num2):
-    return num1 - num2
+	"""subracts one number from the other number"""
+	return num1 - num2
 
 def multiply(num1, num2):
-    return num1 * num2
+	"""multiples two numbers together"""
+	return num1 * num2
 
 def divide(num1, num2):
-    return num1 / num2
+	"""divides one number by another number"""
+	return float(num1) / float(num2)
 
 def square(num1):
-    return num1**2
+	"""Squaring a number"""
+	return num1**2
     
 def cube(num1):
-    return num1**3
+	"""Cubing a number"""
+	return num1**3
 
 def power(num1, num2):
-    return num1 ** num2
+	"""Raising a number to the power of another number"""
+	return num1 ** num2
 
 def mod(num1, num2):
-    return num1 % num2
+	"""Returning the remainder of one number divided by another"""
+	return num1 % num2
